38aeec0efffbacfd0f7960f3768df2014f30ffb8
cherifcoder0 <cherifcoder0@gmail.com>
2026-03-22 03:05:38 +0000

    NBR DE LISTE PREMIER DANSUN INERTVALLE

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function isPrime(nbr) {
     if (nbr >= 2) {
-        for (i = 2; i < nbr; i++) {
+        for (i = 2; i<=Math.sqrt(nbr) ; i++) {//si un nombre a un diviseur supérieur à sa racine carrée, il aura forcément un diviseur inférieur correspondant (les diviseurs vont par paires).
+                                             // Donc inutile de tester jusqu’à nbr - 1, on s’arrête à √nbr.
             if (nbr % i == 0) {
                 return false
             }
